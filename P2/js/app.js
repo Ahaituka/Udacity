b45e0d03aec015652c97d79aec172d3701ec0648
Shubham <bitloks@gmail.com>
2018-04-28 04:21:49 +0530

    List Initialization

--- P2/js/app.js
+++ P2/js/app.js
@@ -1,6 +1,9 @@
 /*
  * Create a list that holds all of your cards
  */
+var allcards= ["fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-leaf", "fa-bicycle", "fa-bomb","fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-leaf", "fa-bicycle", "fa-bomb"];
+
+
 
 
 /*
@@ -24,6 +27,8 @@ function shuffle(array) {
 
     return array;
 }
+
+
 
 
 /*
